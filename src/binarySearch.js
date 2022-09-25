54bd50da03807f2f60368880408baf33b1b79332
Francisco-BT <pancho10810@gmail.com>
2022-09-25 14:01:15 -0500

    feat: add binarySearch recursive version

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -24,10 +24,46 @@ const binarySearch = (array, value) => {
   return undefined;
 };
 
-const valuesToSearch = [1, 10, 90, 100, 1000, 2, 59, -1];
+const valuesToSearch = [1, 10, 90, 100, 1000, 2, 59, -1, 142];
 
 valuesToSearch.forEach((value) => {
   const index = binarySearch(array, value);
+  if (index !== undefined) {
+    console.log(chalk.green(`Index of ${value} is ${index}`));
+  } else {
+    console.log(chalk.red(`Value ${value} is not found in the array`));
+  }
+});
+
+const binarySearchRecursive = (array, value) => {
+  const iteration = (lower = 0, upper = array.length) => {
+    if (lower >= upper) {
+      return undefined;
+    }
+
+    const middle = Math.ceil((upper + lower) / 2);
+    const middleValue = array[middle];
+
+    if (value === middleValue) {
+      return middle;
+    }
+
+    if (value < middleValue) {
+      return iteration(lower, middle - 1);
+    }
+
+    if (value > middleValue) {
+      return iteration(middle + 1, upper);
+    }
+  };
+
+  return iteration();
+};
+
+console.log(chalk.yellowBright('\nUsing binary search recursive version'));
+
+valuesToSearch.forEach((value) => {
+  const index = binarySearchRecursive(array, value);
   if (index !== undefined) {
     console.log(chalk.green(`Index of ${value} is ${index}`));
   } else {
