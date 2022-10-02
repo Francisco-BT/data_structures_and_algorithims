dc715b07f7127ff135e5eea226d454b0f0705536
Francisco-BT <pancho10810@gmail.com>
2022-10-02 17:45:43 -0500

    feat: add bubble sort example

---
+++ src/bubbleSort.js
@@ -0,0 +1,27 @@
+const array = [2, 1, 3, 5];
+
+const bubbleSort = (array) => {
+  let unsortedUntilIndex = array.length - 1;
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < unsortedUntilIndex; i++) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        sorted = false;
+      }
+    }
+    console.log(array);
+    unsortedUntilIndex -= 1;
+  }
+
+  return array;
+};
+
+// console.log('bubbleSort: ', bubbleSort(array));
+console.log(
+  `input: ${[4, 2, 7, 1, 3]}, outupt: ${bubbleSort([4, 2, 7, 1, 3])}`
+);
